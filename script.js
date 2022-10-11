670d107e7854ce1c10342a5166691c84e2c58990
Ermi <manermidem@gmail.com>
2022-10-11 18:37:01 +0300

    Fix nav menu and hamburger

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navi-menu");
-const logo = document.querySelector('.logo');
-const materialIcons = document.querySelector('.material-icons');
+const logo = document.querySelector(".logo");
+const nav = document.querySelector(".nav");
 
 hamburger.addEventListener("click", mobileMenu);
 
@@ -9,7 +9,7 @@ function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
     logo.classList.toggle('active');
-    materialIcons.classList.toggle('active');
+    nav.classList.toggle('active');
 }
 const naviLink = document.querySelectorAll(".navi-link");
 
@@ -18,4 +18,7 @@ naviLink.forEach(n => n.addEventListener("click", closeMenu));
 function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+    logo.classList.remove('active');
+    nav.classList.remove('active');
+
 }
