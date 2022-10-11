0fd29c05e7176fb212eaa1e87d956cf4fc94ee19
Ermi <manermidem@gmail.com>
2022-10-11 16:03:17 +0300

    Edit css and js file

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navi-menu");
+const logo = document.querySelector('.logo');
+const materialIcons = document.querySelector('.material-icons');
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    logo.classList.toggle('active');
+    materialIcons.classList.toggle('active');
 }
 const naviLink = document.querySelectorAll(".navi-link");
 
