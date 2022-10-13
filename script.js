08cfcf6d53c47aace9381f9627f74c0618cbd72c
Ermi <manermidem@gmail.com>
2022-10-13 23:14:29 +0300

    Edit the files for the validation form

--- script.js
+++ script.js
@@ -134,49 +134,35 @@ function closeModal(modal) {
 
 const form = document.querySelector("#form");
 const email = document.getElementById("email");
-const emailError = document.querySelector("#email + span.error");
+const emailError = document.querySelector(".error");
 
-email.addEventListener("input", (event) => {
-  if (email.validity.valid) {
-    // In case there is an error message visible, if the field
-    // is valid, we remove the error message.
-    emailError.textContent = ""; // Reset the content of the message
-    emailError.className = "error"; // Reset the visual state of the message
-  } else {
-    // If there is still an error, show the correct error
-    showError();
-  }
-});
+// email.addEventListener("input", (event) => {
+//   if (email.validity.valid) {
+//     // In case there is an error message visible, if the field
+//     // is valid, we remove the error message.
+//     emailError.textContent = ""; // Reset the content of the message
+//     emailError.className = "error"; // Reset the visual state of the message
+//   } else {
+//     // If there is still an error, show the correct error
+//     showError();
+//   }
+// });
 
 form.addEventListener("submit", (event) => {
-  // if the email field is valid, we let the form submit
-  if (!email.validity.valid) {
-    // If it isn't, we display an appropriate error message
-    showError();
-    // Then we prevent the form from being sent by canceling the event
-    event.preventDefault();
-  }
-});
 
-// if (!email.value.match(/^[a-z\d-\.]+@[a-z\d-]+\.[a-z]{2,8}(\.[a-z]{2,8})?$/)) ^$
-
-
-function showError() {
   if(email.value.match(/[A-Z]/g)) {
+    event.preventDefault();
     emailError.textContent = "Please use lowercases for the email field";
   } else if (email.validity.valueMissing) {
-    // If the field is empty,
-    // display the following error message.
+    event.preventDefault();
     emailError.textContent = "You need to enter an e-mail address.";
   } else if (email.validity.typeMismatch) {
-    // If the field doesn't contain an email address,
-    // display the following error message.
+    event.preventDefault();
     emailError.textContent = "Entered value needs to be an e-mail address.";
   } else if (email.validity.tooShort) {
-    // If the data is too short,
-    // display the following error message.
+    event.preventDefault();
     emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
-  } 
-  
-  emailError.className = "error active";
-}
+  }
+ 
+});
+
