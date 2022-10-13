0d15adba7489cf223adf511b49d0e129ac9ebc5e
Ermi <manermidem@gmail.com>
2022-10-13 21:25:19 +0300

    Add the validation contact form

--- script.js
+++ script.js
@@ -129,3 +129,54 @@ function closeModal(modal) {
     modal.classList.remove('active')
     overlay.classList.remove('active')
 }
+
+// validation contact form
+
+const form = document.querySelector("#form");
+const email = document.getElementById("email");
+const emailError = document.querySelector("#email + span.error");
+
+email.addEventListener("input", (event) => {
+  if (email.validity.valid) {
+    // In case there is an error message visible, if the field
+    // is valid, we remove the error message.
+    emailError.textContent = ""; // Reset the content of the message
+    emailError.className = "error"; // Reset the visual state of the message
+  } else {
+    // If there is still an error, show the correct error
+    showError();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  // if the email field is valid, we let the form submit
+  if (!email.validity.valid) {
+    // If it isn't, we display an appropriate error message
+    showError();
+    // Then we prevent the form from being sent by canceling the event
+    event.preventDefault();
+  }
+});
+
+// if (!email.value.match(/^[a-z\d-\.]+@[a-z\d-]+\.[a-z]{2,8}(\.[a-z]{2,8})?$/)) ^$
+
+
+function showError() {
+  if(email.value.match(/[A-Z]/g)) {
+    emailError.textContent = "Please use lowercases for the email field";
+  } else if (email.validity.valueMissing) {
+    // If the field is empty,
+    // display the following error message.
+    emailError.textContent = "You need to enter an e-mail address.";
+  } else if (email.validity.typeMismatch) {
+    // If the field doesn't contain an email address,
+    // display the following error message.
+    emailError.textContent = "Entered value needs to be an e-mail address.";
+  } else if (email.validity.tooShort) {
+    // If the data is too short,
+    // display the following error message.
+    emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+  } 
+  
+  emailError.className = "error active";
+}
