2bcf3b65f85155db466bd23e07da50b0e3e44c82
Ermi <manermidem@gmail.com>
2022-10-13 11:33:16 +0300

    Edit the script file to make the popup dynamic

--- script.js
+++ script.js
@@ -25,45 +25,62 @@ function closeMenu() {
 
 // pop-up window
 
-const popupHeading = ["Multi Post Stories", "Keeping track of hundreds  of components website"];
-const popImg = ["./images/popupMobile.svg", "./images/live.svg", "./images/GitHub.svg"];
-const popupDescription = [" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita."];
-const popupList = ["html", "Bootstrap", "Ruby on rails"] ;
-const popupLinks = ["https://the-normal-one1.github.io/PortFolio/", "https://github.com/The-Normal-One1/PortFolio"]
+const projectDatas = [
+  {
+  popupHeadingOne: "Multi Post Stories",
+  popImg : "./images/popupMobile.svg",
+  popImgLive: "./images/live.svg",
+  popImgSource: "./images/GitHub.svg",
+  popupDescriptionOne: " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  popupList : ["html", "Bootstrap", "Ruby on rails"],
+  popupLiveLink: "https://the-normal-one1.github.io/PortFolio/",
+  popupSouceLink: "https://github.com/The-Normal-One1/PortFolio"
+}, 
+{
+
+  popupHeadingTwo :"Keeping track of hundreds  of components website",
+  popImg: "./images/popupMobile.svg",
+  popImgLive: "./images/live.svg",
+  popImgSource: "./images/GitHub.svg",
+  popupDescriptionTwo: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+  popupList: ["html", "Bootstrap", "Ruby on rails"],
+  popupLiveLink: "https://the-normal-one1.github.io/PortFolio/",
+  popupSouceLink: "https://github.com/The-Normal-One1/PortFolio"
+}]
 
 const popup = document.querySelector('.modal')
 
 popup.innerHTML = `<div class="pop-up-header">
-<h2 class="popheading1">${popupHeading[0]}</h2>
-<h2 class="popheading2">${popupHeading[1]}</h2>
+<h2 class="popheading1">${projectDatas[0].popupHeadingOne}</h2>
+<h2 class="popheading2">${projectDatas[1].popupHeadingTwo}</h2>
 <button data-close-button class="close-button">&times;</button>
 </div>
 <ul class="pop-up-lists">
 <a class="pop-list" href="#">
-  <li>${popupList[0]}</li>
+  <li>${projectDatas[0].popupList[0]}</li>
 </a>
 <a class="pop-list" href="#">
-  <li>${popupList[1]}</li>
+  <li>${projectDatas[0].popupList[1]}</li>
 </a>
 <a class="pop-list" href="#">
-  <li>${popupList[2]}</li>
+  <li>${projectDatas[0].popupList[2]}</li>
 </a>
 </ul>
 <div class="popcon">
-<img src="${popImg[0]}" alt="popup" />
+<img src="${projectDatas[0].popImg}" alt="popup" />
 <div class="poptextbutton">
 <p class="popuppharagraph1">
-${popupDescription[0]}
+${projectDatas[0].popupDescriptionOne}
 </p>
 <p class="popuppharagraph2">
-${popupDescription[1]}
+${projectDatas[1].popupDescriptionTwo}
 </p>
 <div class="pop-up-btns">
-<a class="pop-btn" href="${popupLinks[0]}">
-  <p>See Live</p> <img src="${popImg[1]}" alt="Live" />
+<a class="pop-btn" href="${projectDatas[0].popupLiveLink}">
+  <p>See Live</p> <img src="${projectDatas[0].popImgLive}" alt="Live" />
 </a>
-<a class="pop-btn" href="${popupLinks[1]}">
-  <p>See Source</p> <img src="${popImg[2]}" alt="Github" />
+<a class="pop-btn" href="${projectDatas[0].popupSouceLink}">
+  <p>See Source</p> <img src="${projectDatas[0].popImgSource}" alt="Github" />
 </a>
 </div>
 </div>
